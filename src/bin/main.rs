50245701ef1f4872a7aee9999c684eed67cdd60f
RyoGreen <41415120ryo@gmail.com>
2024-10-14 22:07:16 -0400

    multiple thread server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,19 @@
-use std::fs;
+use rust_cargo_practice::ThreadPool;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::{fs, thread, time::Duration};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(5);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        })
     }
 }
 
@@ -18,11 +24,15 @@ fn handle_connection(mut stream: TcpStream) {
         return;
     }
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(10));
+        ("HTTP/1.1 200 OK", "index.html")
     } else {
-        ("HTTP/1.1 200 OK", "404.html")
+        ("HTTP/1.1 404 Not Found", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
