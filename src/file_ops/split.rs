04d51335c8279ca3f72f2bc217c598f4f107b310
RyoGreen <41415120ryo@gmail.com>
2024-11-25 23:06:36 -0500

    struct chunk and file info

--- src/file_ops/split.rs
+++ src/file_ops/split.rs
@@ -1,12 +1,24 @@
 use anyhow::{Context, Result};
+use sha2::{Digest, Sha256};
 use std::fs::{self, File};
 use std::io::{Read, Write};
 use std::path::Path;
-
 const CHUNK_SIZE: usize = 1024;
 const OUTPUT_DIR: &str = "output_chunks";
 
-pub fn split_file(input_file: &str) -> Result<()> {
+pub struct FileInfo {
+    hash: String,
+    file_name: String,
+    updated_use_address: String,
+    file_chunk: Vec<FileChunk>,
+}
+
+pub struct FileChunk {
+    file_name: String,
+    chunk_hash: String,
+}
+
+pub fn split_file(input_file: &str, updated_user_address: &str) -> Result<(FileInfo)> {
     let input_path = Path::new(input_file);
     let file_extension = input_path
         .extension()
@@ -20,6 +32,7 @@ pub fn split_file(input_file: &str) -> Result<()> {
     let mut file = File::open(input_file)?;
     let mut buffer = vec![0; CHUNK_SIZE];
     let mut chunk_index = 0;
+    let mut chunks = Vec::new();
     while let Ok(bytes_read) = file.read(&mut buffer) {
         if bytes_read == 0 {
             break;
@@ -28,7 +41,26 @@ pub fn split_file(input_file: &str) -> Result<()> {
         let chunk_filename = format!("{}/chunk_{}.{}", OUTPUT_DIR, chunk_index, file_extension);
         let mut chunk_file = File::create(&chunk_filename)?;
         chunk_file.write_all(&buffer[..bytes_read])?;
+
+        let chunk_hash = calculate_hash(&buffer[..bytes_read]);
+
+        chunks.push(FileChunk {
+            file_name: chunk_filename.clone(),
+            chunk_hash,
+        });
+
         chunk_index += 1;
     }
-    Ok(())
+    Ok(FileInfo {
+        hash: calculate_hash(&fs::read(input_path)?),
+        file_name: input_file.to_string(),
+        updated_use_address: updated_user_address.to_string(),
+        file_chunk: chunks,
+    })
+}
+
+fn calculate_hash(data: &[u8]) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    format!("{:x}", hasher.finalize())
 }
